3bfa3cb4a0fb3e2f7f366ed6aa59425e19211864
Anmol Joshi <anmolsjoshi@gmail.com>
2018-05-24 08:13:14 -0700

    Updated install.sh

--- install.sh
+++ install.sh
@@ -13,4 +13,16 @@ source ./env/bin/activate
 echo "Installing Requirements"
 pip install -r requirements.txt
 
+echo "Adding repos to lib"
+mkdir lib
+cd lib
+git clone https://github.com/anmolsjoshi/KerasHelpers.git
+cp KerasHelpers/__init__.py .
+cd .
+
+mkdir data/raw/wordvectors
+cd data/raw/wordvectors
+
+
 ipython kernel install --user --name=textCNN.keras
+
